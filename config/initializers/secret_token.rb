812de3b97aa73d0677563d6ed34c746b4fc40b86
dcoli <github@macallister.net>
2011-12-24 08:16:09 -0500

    everything else

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WakeyWakey::Application.config.secret_token = 'c1e0eef2d23c9c22e2778cb598badcf76839b9d332dbd7b0118d51b8f7db7749f0b7a93af52489d92990dd2b55e4c875a184ed2aeecf417354c727c247fab880'
